806ba1ef2c2e37c41ddaffc01230a2dc17d5ee57
teddywaweru <teddywaweru@gmail.com>
2022-06-14 23:53:44 +0300

    220614 - Progress Update

--- test_1.py
+++ test_1.py
@@ -1,13 +1,1 @@
-def get_primes(n):
-    multipliers = []
-    multiplier = 2
-    while n!=1:
-        if n % multiplier != 0:
-            multiplier += 1
-            continue
-        n //= multiplier
-        multipliers.append(multiplier)
-
-    return multipliers
-
-print(get_primes(630))
+impor
